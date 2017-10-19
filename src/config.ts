5dc01630be6ccde5f8c1958167a11131d7d7c111
Lukasz Klis <lukasz@klis.pl>
2017-10-19 10:51:34 +0200

    :hammer: Use TypeScript

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+import * as fse from 'fs-extra';
+import * as path from 'path';
+import * as yaml from 'js-yaml';
+
+interface Settings {
+  lectureIds: number[];
+}
+
+export const buildFilePath = path.resolve(__dirname, '../build/user-styles.css');
+export const templateFilePath = path.resolve(__dirname, '../templates/user-styles.template.css');
+
+export function getSettings(): Settings {
+  try {
+    const settingsFilePath = path.resolve(__dirname, '../settings.yaml');
+
+    return yaml.safeLoad(fse.readFileSync(settingsFilePath, 'utf8'));
+  } catch (error) {
+    throw new Error('Could not fetch the settings.yaml file. Make sure you’ve copied settings.yaml.dist and renamed it to settings.yaml.');
+  }
+}
